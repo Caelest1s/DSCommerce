46642ee9929fb306067aaf10f03b6927e108327a
Caelest1s <jefferson.ucb02@gmail.com>
2023-10-17 20:25:33 -0300

    Exception handling

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,16 +1,20 @@
 package com.devsuperior.dscommerce.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.DatabaseException;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 //Poderia utilizar o @Component também porém o Service tem uma melhor semântica
 @Service
@@ -31,10 +35,12 @@ public class ProductService {
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
 
-        Optional<Product> result = repository.findById(id);
-        Product product = result.get();
+        // Optional<Product> result = repository.findById(id);
+        Product result = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
+        // Product product = result.get();
 
-        ProductDTO dto = new ProductDTO(product);
+        ProductDTO dto = new ProductDTO(result);
         /**
          * ProductDTO dto = new ProductDTO(product);
          * 
@@ -70,15 +76,38 @@ public class ProductService {
     @Transactional
     public ProductDTO update(Long id, ProductDTO dto) {
 
-        //'getReferencedById' não vai no banco só prepara o OBJ, monitorado pelo JPA
-        Product entity = repository.getReferenceById(id);
-        copyDtoToEntity(dto, entity);
-        entity = repository.save(entity);
-
-        return new ProductDTO(entity);
+        // 'getReferencedById' não vai no banco só prepara o OBJ, monitorado pelo JPA
+        try {
+            Product entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+    
+            return new ProductDTO(entity);
+        } 
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
     }
 
-    //replicou 2x já é o suficiente para criar um método
+    // propagation é para quando o JAVA depende do JDBC que o impossibilita
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+
+        try {
+            repository.deleteById(id);
+
+        } 
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+
+    }
+
+    // replicou 2x já é o suficiente para criar um método
     private void copyDtoToEntity(ProductDTO dto, Product entity) {
 
         entity.setName(dto.getName());
@@ -86,11 +115,4 @@ public class ProductService {
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
     }
-
-    @Transactional
-    public void delete(Long id) {
-
-        repository.deleteById(id);
-    }
-
 }
